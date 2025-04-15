3a8ae8adc7caa195b3f5f50718ce422c531e9d06
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2025-04-15 15:47:10 -0700

    main script now supports reading text from file and color coding matches

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
-import sys
-import regex
+import argparse, regex
+
+
+def colorMatches(matches: list[regex.Match], text: str) -> str:
+    BOLD_RED = "\x1b[31;1m"
+    RESET = "\x1b[0m"
+    
+    colorStr = ""
+
+    prevEnd = 0
+    for m in matches: 
+        colorStr += text[prevEnd:m.start]
+        colorStr += f"{BOLD_RED}{text[m.start:m.end]}{RESET}"
+        prevEnd = m.end
+
+    colorStr += text[prevEnd:]
+
+    return colorStr
+                
+
+def readStdin(args: argparse.Namespace):
+    re = regex.RE(args.PATTERN)
+    
+    while True:
+        try:
+            text = input()
+        except EOFError:
+            break
+        else:
+            matches, ok = re.matchPattern(text)
+            if not ok:
+                print("> No matches")
+            else:
+                print(colorMatches(matches, text))
+
+
+def readFile(args: argparse.Namespace):
+    re = regex.RE(args.PATTERN)
+
+    with open(args.file) as f:
+        for lineNum, line in enumerate(f, start=1):
+            line = line.strip()
+            matches, ok = re.matchPattern(line)
+            if ok:
+                print(f"{lineNum}: {colorMatches(matches, line)}")
 
 
 def main():
-    if len(sys.argv) < 3:
-        print("Expected 2 arguments")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("PATTERN", help="regular expression pattern")
+    parser.add_argument("-f", "--file", help="file to search for all occurences of pattern. If no file is provided read from stdin")
 
-    pattern = sys.argv[2]
-    input_line = input()
-
-    if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
-        exit(1)
-
-    r = regex.RE(pattern)
-    _, matched = r.matchPattern(input_line)
-    if matched:
-        exit(0)
+    args = parser.parse_args()
+    
+    if args.file != None:
+        readFile(args)
     else:
-        exit(1)
-
-
-
+        readStdin(args)
 
 if __name__ == "__main__":
     main()
