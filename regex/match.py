edbb35c902e32793c16201a9ed78909280e92fef
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2026-03-29 10:44:49 -0700

    added .group() method to Match object

--- regex/match.py
+++ regex/match.py
@@ -7,12 +7,35 @@ class Match:
     span: tuple[int, int]
     captures: dict[int, tuple[int, int]]
     string: str
+    _num_groups: int
 
     def start(self) -> int:
         return self.span[0]
 
     def end(self) -> int:
         return self.span[1]
+
+    def group(self, *args: int) -> tuple[str | None, ...] | str | None:
+        for group_num in args:
+            if group_num < 0 or group_num > self._num_groups:
+                raise IndexError("no such group")
+
+        res = []
+
+        for group_num in args:
+            span = self.captures.get(group_num, None)
+
+            if span is not None:
+                res.append(self.string[span[0] : span[1]])
+            elif group_num == 0:
+                res.append(self.string[self.start() : self.end()])
+            else:
+                res.append(None)
+
+        if len(res) == 1:
+            return res[0]
+        
+        return tuple(res)
 
 
 @dataclass(frozen=True)
