68c516bc51742eceb4268955ef79f96b073d3d37
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2026-03-29 11:52:16 -0700

    updated Match class

--- regex/match.py
+++ regex/match.py
@@ -1,13 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(frozen=True)
 class Match:
     match: str
     span: tuple[int, int]
     captures: dict[int, tuple[int, int]]
     string: str
-    _num_groups: int
+    _num_groups: int = field(repr=False)  # private field, excluded from repr
 
     def start(self) -> int:
         return self.span[0]
@@ -28,13 +28,13 @@ class Match:
             if span is not None:
                 res.append(self.string[span[0] : span[1]])
             elif group_num == 0:
-                res.append(self.string[self.start() : self.end()])
+                res.append(self.match)
             else:
                 res.append(None)
 
         if len(res) == 1:
             return res[0]
-        
+
         return tuple(res)
 
 
@@ -44,6 +44,3 @@ class MatchState:
     pos: int
     # Keys are group ID's and values are the start and end index of captured group
     captures: dict[int, tuple[int, int]]
-
-    def __hash__(self):
-        return hash((self.pos, tuple(sorted(self.captures.items()))))
